8d02cd79ce61ac1ef817e4ed581422a7e98ccf0a
lurbani <ctt.urbani@gmail.com>
2023-10-19 00:07:15 -0300

    validacion de campos de formulario

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,32 @@
 function validar(){
-    var nombre = document.getElementById("nombre").trim();
-    var email = document.getElementById("email").trim();
-    var tel = document.getElementById("phone").trim();
-    var consulta = document.getElementById("consulta").trim();
-    alert(nombre + email + tel + consulta)
+    var nombre = document.getElementById("nombre").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var tel = document.getElementById("phone").value.trim();
+    var consulta = document.getElementById("consulta").value.trim();
+
+    console.log(nombre)
+    console.log(email)
+    console.log(tel)
+    console.log(consulta)
+    if(email ==="" && tel==="")
+    {
+        alert("Debe proveer un medio de contacto");
+        return false;
+    }
+    else if(tel !=="" && isNaN(tel))
+    {
+        alert("Numero de telefono invalido");
+        return false;
+    }
+    else if(consulta === "")
+    {
+        alert("Debe hacer una consulta");
+        return false;
+    }
+    else
+    {
+        alert("Consulta enviada");
+        return true;
+    }
+    
 }
