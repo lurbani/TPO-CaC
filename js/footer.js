ad25662616e0365bb127afba9d2c87c6c1477870
lurbani <ctt.urbani@gmail.com>
2023-12-02 01:00:56 -0300

    puedo traer productos desde la api. El header y el footer se renderiza con un script

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("footer")[0].innerHTML= `
+<div>
+<img src="/img/socmedia_icons/fb.png" alt="fb icon">
+<img src="/img/socmedia_icons/ig.png" alt="ig icon">
+<h6 class="Footer">Grupo 26 - 2023</h6>
+</div>
+
+`
